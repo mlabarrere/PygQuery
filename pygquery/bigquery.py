a6a69ca7eda3133bfd89a20a5e1b2fbd90d5e9bd
Mickael Labarrere <35226465+mlabarrere@users.noreply.github.com>
2020-12-26 14:27:32 +0100

    Update Struct

--- pygquery/bigquery.py
+++ pygquery/bigquery.py
@@ -1,5 +1,5 @@
-"""BigQueryReader
-Reads a query
+"""BigQueryWrapper
+Reads & Writes query
 """
 
 from threading import Thread
@@ -42,3 +42,36 @@ class BigQueryReader(Thread):
     def data(self):
         """return data"""
         return self._data
+
+
+class BigQueryWriter(Thread):
+    """Reads Query"""
+    def __init__(self, dataframe, project, dataset, table, if_exists,
+                 api_key_path):
+        """Object Configuration"""
+        Thread.__init__(self)
+        credentials = Credentials.from_service_account_file(api_key_path)
+        _client = bigquery.Client(credentials=credentials, project=project)
+
+        dataset_ref = _client.dataset(dataset)
+        table_ref = dataset_ref.table(table)
+        """Job Configuraton"""
+        _job_config = bigquery.LoadJobConfig()
+        _job_config.autodetect = True
+
+        if if_exists == 'append':
+            _job_config.write_disposition = 'WRITE_APPEND'
+        elif if_exists == 'overwrite':
+            _job_config.write_disposition = 'WRITE_TRUNCATE'
+        else:
+            _job_config.write_disposition = 'WRITE_EMPTY'
+
+        self._query_job = _client.load_table_from_dataframe(
+            dataframe=dataframe,
+            destination=table_ref,
+            project=project,
+            job_config=_job_config)
+
+    def run(self):
+        """starts writing data"""
+        self._query_job.result()
