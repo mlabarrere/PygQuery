e1e30651befa3a7f8630598fd6657d63ec35b6b7
Mickael Labarrere <35226465+mlabarrere@users.noreply.github.com>
2020-12-26 12:57:32 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="PygQuery",
-    version="0.0.1",
+    version="0.0.2",
     author="M_Lbr",
     author_email="m.micky.lbr@gmail.com",
     description="Utils package for Google BigQuery",
